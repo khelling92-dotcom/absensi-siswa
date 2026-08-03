70cf75042bbb50fa98c2db5ebe45ca55a1132a8b
khelling92-dotcom <khelling92@gmail.com>
2026-08-03 20:47:21 +0700

    Update cetak_rekap_dhuha.php

--- cetak_rekap_dhuha.php
+++ cetak_rekap_dhuha.php
@@ -96,7 +96,7 @@ $profil = mysqli_fetch_assoc(
 ========================== */
 
 $waliNama = "................................";
-$waliNip  = "................................";
+$waliNbm  = "................................";
 
 if($kelas!=""){
 
@@ -112,7 +112,7 @@ if($kelas!=""){
         $w=mysqli_fetch_assoc($qWali);
 
         $waliNama=$w['nama_wali'];
-        $waliNip=$w['nip_wali'];
+        $waliNip=$w['nbm_wali'];
 
     }
 
@@ -484,8 +484,8 @@ Kepala Sekolah
 
 <br>
 
-NIP.
-<?= htmlspecialchars($profil['nip_kepala'] ?? '-') ?>
+NBM.
+<?= htmlspecialchars($profil['nbm_kepala'] ?? '-') ?>
 
 </td>
 
@@ -510,8 +510,8 @@ Wali Kelas
 
 <br>
 
-NIP.
-<?= htmlspecialchars($waliNip) ?>
+NBM.
+<?= htmlspecialchars($waliNbm) ?>
 
 </td>
 
